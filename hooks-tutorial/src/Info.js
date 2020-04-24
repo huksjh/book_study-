561c4506acbe57c9062e95257a86fda29d1432e4
Your Name <you@example.com>
2020-04-24 21:55:42 +0900

    커밋내용 입력하면 전부 바뀌나?

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
-
+    useEffect(() => {
+        console.log('렌더링됨');
+        console.log({
+            name,
+            nickname,
+        });
+    });
     const onChangeName = (e) => {
         setName(e.target.value);
     };
