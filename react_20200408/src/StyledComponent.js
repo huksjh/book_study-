a47800158da426af1e4d9d68cc8d82a9da55c725
Your Name <you@example.com>
2020-04-26 22:14:36 +0900

    todo-app test

--- react_20200408/src/StyledComponent.js
+++ react_20200408/src/StyledComponent.js
@@ -4,7 +4,16 @@ import styled, { css } from 'styled-components';
 const Box = styled.div`
     background: ${(props) => props.color || 'blue'};
     padding: 1rem;
-    display: block;
+    display: flex;
+
+    width: 1024px;
+    margin: 0 auto;
+    @media (max-width: 1024px) {
+        width: 768px;
+    }
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 const Button = styled.button`
---
+++ todo-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import './App.css';
+
+function App() {
+    return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+}
+
+export default App;
