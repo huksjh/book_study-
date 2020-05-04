910b2c34cb13cb5f28ca1a3f9eedd94525120677
Your Name <you@example.com>
2020-05-05 00:55:58 +0900

    react-virtualized

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-    console.log(todos);
-    return (
-        <div className="TodoList">
-            {todos.map((todo) => (
-                /*      
-                console.log(todo);
-                result :   
-                {id: 1, text: "리액트의 기초 알아보기", checked: true}
-                {id: 2, text: "컴포넌트 스타일링해 보기", checked: true}
-                {id: 3, text: "일정 관리 앱 만들어 보기", checked: false} */
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-            ))}
-        </div>
+    //console.log(todos);
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style} />;
+        },
+        [onRemove, onToggle, todos]
     );
+    return <List className="TodoList" width={512} height={513} rowCount={todos.length} rowHeight={57} rowRenderer={rowRenderer} list={todos} style={{ outline: 'none' }} />;
 };
 
-export default TodoList;
+export default React.memo(TodoList);
