399edbc978e2602910802f3ff8c112c0aff0161f
Your Name <you@example.com>
2020-05-04 23:22:07 +0900

    10장 클

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback((e) => {
+        console.log(e.target.value);
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        (e) => {
+            onInsert(value); //app.js 에서 props로 넘어온 함수 onInsert 에 value값 전달
+            setValue(''); //value 값 초기화
+            //submit 이벤트는 브라우저에서 새로고침을 발생
+            //방지 하기위해 사용
+            e.preventDefault();
+        },
+        [onInsert, value]
+    );
     return (
-        <form className="TodoInsert">
-            <input placeholders="할 일을 입력하세요" />
-            <button type="button">
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholders="할 일을 입력하세요" value={value} onChange={onChange} />
+            <button type="submit">
                 <MdAdd />
             </button>
         </form>
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,12 +2,19 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
+    console.log(todos);
     return (
         <div className="TodoList">
-            <TodoListItem />
-            <TodoListItem />
-            <TodoListItem />
+            {todos.map((todo) => (
+                /*      
+                console.log(todo);
+                result :   
+                {id: 1, text: "리액트의 기초 알아보기", checked: true}
+                {id: 2, text: "컴포넌트 스타일링해 보기", checked: true}
+                {id: 3, text: "일정 관리 앱 만들어 보기", checked: false} */
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
+            ))}
         </div>
     );
 };
